884a76acb260e3bdd78e30566c34b3257416ddac
jChambord <70707261+jeromeChamb@users.noreply.github.com>
2021-03-26 21:39:06 +0100

    Update CarReduce.java

--- MapReduce/java/CarReduce.java
+++ MapReduce/java/CarReduce.java
@@ -13,23 +13,11 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.util.GenericOptionsParser;
-    
-    // Notre classe REDUCE.
-	// Les 4 types generques correspondent a:
-	// 1 - Text: C'est le type de la cle d'entre.
-	// 2 - Text: C'est le type de la valeur d'entre.
-	// 3 - Text: C'est le type de la cle de sortie.
-	// 4 - Text: C'est le type de la valeur de sortie.
+   
 	
 public class CarReduce extends Reducer<Text, Text, Text, Text> {
-	// La fonction REDUCE.
-	// Les arguments:
-	//   La cle key,
-	//   Un Iterable de toutes les valeurs qui sont associees a la cle en question
-	//   Le contexte Hadoop (un handle qui nous permet de renvoyer le resultat a Hadoop).
-	// Note: Le type du premier argument correspond au premier type generique.
-	// Note: Le type du second argument Iterable correspond au deuxieme type generique.
-	// Note: L'objet Context nous permet d'ecrire les couples (cle,valeur).
+	
+	
 	public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
 		
 		String malus_bonus;
